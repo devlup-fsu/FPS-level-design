a1495692d3272524edf2f0129fd26dad708a954d
Dion Tryban <github@diontryban.com>
2024-03-21 16:16:05 -0400

    Fix level loading between different OS's

--- scripts/MultiplayerSpawner.gd
+++ scripts/MultiplayerSpawner.gd
@@ -10,7 +10,12 @@ func dir_contents(path):
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
+		var file_names = []
 		while file_name != "":
 			if not dir.current_is_dir() and file_name.split('.')[1] == "tscn":
-				add_spawnable_scene("res://levels/" + file_name)
+				file_names.append(file_name)
 			file_name = dir.get_next()
+		
+		file_names.sort()
+		for f_name in file_names:
+			add_spawnable_scene("res://levels/" + f_name)
